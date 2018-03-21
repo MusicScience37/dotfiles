85132554e8d5cb7ee8705d892186dde51b5af4d5
Kenta Kabashima (Virtual Ubuntu on Home PC 2) <kenta_program37@hotmail.co.jp>
2018-03-21 23:25:42 +0900

    comment out lines

--- bash_init.sh
+++ bash_init.sh
@@ -1,11 +1,11 @@
-export EIGEN_DIR=/usr/local/Eigen/Eigen_3_3_4
+#export EIGEN_DIR=/usr/local/Eigen/Eigen_3_3_4
 
-export CC=gcc-7
-export CXX=g++-7
-alias gcc=gcc-7
-alias g++=g++-7
+#export CC=gcc-7
+#export CXX=g++-7
+#alias gcc=gcc-7
+#alias g++=g++-7
 
-export MPI_ROOT=/usr/local/openmpi
-export PATH=$MPI_ROOT/bin:$PATH
-export LD_LIBRARY_PATH=$MPI_ROOT/lib:$LD_LIBRARY_PARH
-export MANPATH=$MPI_ROOT/share/man:$MANPATH
+#export MPI_ROOT=/usr/local/openmpi
+#export PATH=$MPI_ROOT/bin:$PATH
+#export LD_LIBRARY_PATH=$MPI_ROOT/lib:$LD_LIBRARY_PARH
+#export MANPATH=$MPI_ROOT/share/man:$MANPATH
