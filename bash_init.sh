adccfb0a0496711d9188b29632cc28abd20ad11a
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2019-12-12 22:36:19 +0900

    feat: add a script to view the current branch in git repositories

--- bash_init.sh
+++ bash_init.sh
@@ -1,11 +1,6 @@
-#export EIGEN_DIR=/usr/local/Eigen/Eigen_3_3_4
+git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
 
-#export CC=gcc-7
-#export CXX=g++-7
-#alias gcc=gcc-7
-#alias g++=g++-7
+export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;32m\]\$(git_branch)\[\033[00m\]\$ "
 
-#export MPI_ROOT=/usr/local/openmpi
-#export PATH=$MPI_ROOT/bin:$PATH
-#export LD_LIBRARY_PATH=$MPI_ROOT/lib:$LD_LIBRARY_PARH
-#export MANPATH=$MPI_ROOT/share/man:$MANPATH
